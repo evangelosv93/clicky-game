118aa95ce55ec76daf7e35f6e9be48a66fabc1b9
evangelosv93 <evan7@umbc.edu>
2018-04-23 09:49:44 -0400

    firsty

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Random clicks</h1>
+    <h2>
+      Click on an image to earn points, lets see if this works!
+    </h2>
+  </header>
+);
+
+export default Header;
